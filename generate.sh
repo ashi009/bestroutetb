75fa1ac11887b91e10cf5fa65534baa29c07aba5
SHI Xiaoyi <ashi009@gmail.com>
2012-11-27 11:38:04 +0800

    Fixed wget command typo

--- generate.sh
+++ generate.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ ! -e delegated-apnic-latest.dat ]; then
-  wget -o delegated-apnic-latest.dat http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest
+  wget -O delegated-apnic-latest.dat http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest
 fi
 
 if [ ! -e ipv4-address-space.dat ]; then
-  wget -o ipv4-address-space.dat http://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.txt
+  wget -O ipv4-address-space.dat http://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.txt
 fi
 
 filename=$1
