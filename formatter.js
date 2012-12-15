4c3ea46d5ed4934eb68c14291fcf2137db621404
SHI Xiaoyi <ashi009@gmail.com>
2012-12-15 22:16:41 +0800

    Added: support header and footer in the output.

--- formatter.js
+++ formatter.js
@@ -15,26 +15,32 @@ var kProfiles = {
     }
   },
   route_up: {
+    header: '#!/bin/sh',
     format: 'route add %prefix netmask %mask gw %gw',
     gw: kDefaultGateway
   },
   route_down: {
+    header: '#!/bin/sh',
     format: 'route delete %prefix',
     gw: kDefaultGateway
   },
   iproute_up: {
+    header: '#!/bin/sh',
     format: 'ip r a %prefix/%length via %gw',
     gw: kDefaultGateway
   },
   iproute_down: {
+    header: '#!/bin/sh',
     format: 'ip r d %prefix',
     gw: kDefaultGateway
   },
   win_up: {
+    header: '@echo off',
     format: 'route add %prefix mask %mask %gw',
     gw: kDefaultGateway
   },
   win_down: {
+    header: '@echo off',
     format: 'route delete %prefix',
     gw: kDefaultGateway
   },
@@ -47,8 +53,14 @@ var kProfile = kProfiles.hasOwnProperty(opts.profile) ?
     kProfiles[opts.profile] : kProfiles.openvpn;
 
 lib.getRulesFromInput(function(rules) {
+  var header = opts.header || kProfile.header;
+  if (header)
+    console.log(header);
   rules.forEach(function(rule) {
     rule.gw = kProfile.gw[rule.gw];
     console.log(kProfile.format.format(rule, opts));
   });
+  var footer = opts.footer || kProfile.footer;
+  if (footer)
+    console.log(footer);
 });
