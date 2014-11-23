4181cc376ccaccbd226e68ad3257bdff0a8ad82d
Xiaoyi Shi <ashi009@gmail.com>
2014-11-23 23:51:16 +0800

    Export more apis

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = {
+  Db: require('./lib/db'),
   Minifier: require('./lib/minifier'),
   Formatter: require('./lib/formatter'),
+  Profile: require('./lib/profile'),
   Evaluator: require('./lib/evaluator')
 };
