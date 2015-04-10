87650bd6a8093f113eb1e0acf5164f9e398c5fa3
zheng1 <leofml2012@gmail.com>
2015-04-10 23:41:25 +0800

    According to ashi009/bestroutetb#27

--- profile/mac.js
+++ profile/mac.js
@@ -7,17 +7,15 @@ $inherit(MacUpFormatter, Formatter, {
     var up = [];
     up.push('#!/bin/sh');
     up.push('export PATH="/bin:/sbin:/usr/sbin:/usr/bin"');
-    up.push('OLDGW=`netstat -nr | grep "^default" | grep -v "ppp" | sed "s/default *\\([0-9\\.]*\\) .*/\\1/"`');
+    up.push('OLDGW=`netstat -nr | grep "^default" | grep -v "ppp" | awk -v N=$N "{print $2}"');
     up.push('dscacheutil -flushcache');
-    // up.push('if [ ! -e /tmp/pptp_oldgw ]; then');
     up.push('echo $OLDGW > /tmp/pptp_oldgw');
     up.push('route add 10.0.0.0/8 $OLDGW');
     up.push('route add 172.16.0.0/12 $OLDGW');
     up.push('route add 192.168.0.0/16 $OLDGW');
-    // up.push('fi');
     rules.forEach(function(rule) {
       if (rule.gateway === 'vpn') {
-        up.push('route add ' + rule.prefix + '/' + rule.length + ' -interface $0');
+        up.push('route add ' + rule.prefix + '/' + rule.length + ' $4');
       } else {
         up.push('route add ' + rule.prefix + '/' + rule.length + ' $OLDGW');
       }
@@ -25,30 +23,30 @@ $inherit(MacUpFormatter, Formatter, {
     return up.join('\n');
   }
 });
+
 function MacDownFormatter() {
 }
 $inherit(MacDownFormatter, Formatter, {
   format: function(rules) {
     var up = [];
     up.push('#!/bin/sh');
-    up.push('if [ ! -e /tmp/pptp_oldgw ]; then');
-    up.push('    exit 0');
-    up.push('fi');
+    up.push('[ -s /tmp/pptp_oldgw ] || exit 1');
     up.push('OLDGW=`cat /tmp/pptp_oldgw`');
     up.push('route delete 10.0.0.0/8 $OLDGW');
     up.push('route delete 172.16.0.0/12 $OLDGW');
     up.push('route delete 192.168.0.0/16 $OLDGW');
     rules.forEach(function(rule) {
       if (rule.gateway === 'vpn') {
-        up.push('route delete ' + rule.prefix + '/' + rule.length + ' -interface $0');
+        up.push('route delete ' + rule.prefix + '/' + rule.length + ' $4');
       } else {
         up.push('route delete ' + rule.prefix + '/' + rule.length + ' $OLDGW');
       }
     });
-    up.push('rm /tmp/pptp_oldgw');
+    up.push('rm -f /tmp/pptp_oldgw');
     return up.join('\n');
   }
 });
+
 return {
   'ip-up': {
     mode: 0777,
